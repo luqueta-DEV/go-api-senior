f5e65b0f4f0174f35decdd39eb6c980105a8b898
luqueta-DEV <lucasaraujo080806@gmail.com>
2025-02-02 15:18:33 -0300

    projeto atualizado nivel junior

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module go-project
 
 go 1.23.5
+
+require (
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.24 // indirect
+)
--- main.go
+++ main.go
@@ -1,22 +1,60 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "hello yatan")
+type User struct {
+	ID    uint   `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
 
+var db *gorm.DB
+
+func init() {
+	var err error
+	db, err = gorm.Open("sqlite3", "./gorm.db")
+	if err != nil {
+		fmt.Println("Erro ao conectar ao banco de dados")
+		panic(err)
+	}
+
+	db.AutoMigrate(&User{})
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	var user User
+	r.ParseForm()
+	user.Name = r.FormValue("name")
+	user.Email = r.FormValue("email")
+
+	db.Create(&user)
+
+	fmt.Fprintf(w, "Usuário criado: %+v", user)
+}
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	var users []User
+	db.Find(&users)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(users)
 }
 
 func main() {
 
-	http.HandleFunc("/", home)
+	http.HandleFunc("/users/create", createUser)
 
-	fmt.Println("server ta rodando lisin meu nobre na porta 8080")
+	http.HandleFunc("/users", getUsers)
+
+	fmt.Println("Servidor rodando na porta 8080...")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
-		fmt.Println("error", err)
+		fmt.Println("Erro ao iniciar o servidor:", err)
 	}
-
 }
